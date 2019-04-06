7bc6a017c5049d8af5dd9106b42d0e8251c115b8
Alex <alexckolosov@gmail.com>
2019-04-06 09:42:07 +0900

    add functions unzip and cp_n_files

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,23 @@
+import os
 import re
+import shutil
+import zipfile
 
 
-def extract_file_id(fname):
+def extract_file_id(fname: str) -> int:
 
     print("Extracting id from " + fname)
     return int(re.search('\d+', fname).group())
+
+
+def unzip(archive_name: str, extract_path: str) -> None:
+
+    zip_ref = zipfile.ZipFile(archive_name, 'r')
+    zip_ref.extractall(extract_path)
+    zip_ref.close()
+
+
+def cp_n_files(src_dir, dst_dir, n):
+    files = os.listdir(src_dir)[:n]
+    for f in files:
+        shutil.copy(os.path.join(src_dir, f), os.path.join(dst_dir, f))
